79e6c78f02c1a45ab5e387290b39b71403fdd589
lcdavison <ld479@ln-moco-02-10.1.68.25.brighton.ac.uk>
2019-02-15 15:01:47 +0000

    Added Main Menu UI handler

---
+++ UnityProject/Assets/Source/MainMenu/MainMenuUIHandler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuUIHandler : MonoBehaviour
+{
+    public void PlayOnClick()
+    {
+        //  TODO: Use SceneManager to load level
+        Debug.Log ( "Play Clicked" );
+    }
+}
