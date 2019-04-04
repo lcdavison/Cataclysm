5736716fdceed3c473b705cc7c6e8c80314bd194
lcdavison <luke.davison@talktalk.net>
2019-04-04 15:07:35 +0100

    Added doors and spotlights

---
+++ UnityProject/Assets/Source/WorldObjects/Door.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    private float door_speed;
+
+    [SerializeField]
+    private Transform destination_marker;
+    private Transform door;
+
+    private Vector3 original_position;
+    private Vector3 destination;
+
+    void Start ( )
+    {
+        door = transform.parent.gameObject.transform.GetChild ( 0 ).transform;
+        original_position = door.position;
+        destination = door.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( destination != door.position )
+        {
+            door.position = Vector3.Lerp ( door.position, destination, door_speed * Time.deltaTime );
+        }
+    }
+
+    void OnTriggerEnter ( Collider collider )
+    {
+        if ( collider.gameObject.tag == "Player" )
+        {
+            destination = destination_marker.position;
+        }
+    }
+
+    void OnTriggerExit ( Collider collider )
+    {
+        if ( collider.gameObject.tag == "Player" )
+        {
+            destination = original_position;
+        }
+    }
+}
