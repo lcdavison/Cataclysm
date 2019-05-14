a0dab4962a280683250d8a683672d4300e1a7c55
lcdavison <luke.davison@talktalk.net>
2019-05-14 12:23:33 +0100

    Projectile hit detection

--- UnityProject/Assets/Source/Weapons/Projectile.cs
+++ UnityProject/Assets/Source/Weapons/Projectile.cs
@@ -4,32 +4,54 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    [SerializeField]
-    private float lifetime = 1.0f;
+    private float lifetime = 0.4f;
 
     [SerializeField]
     private float projectile_speed = 200.0f;
 
-    private float death_time;
+    private float spawn_time;
+
+    private float damage;
 
     private Rigidbody rigidbody;
 
     // Start is called before the first frame update
-    void Start()
+    void Start ( )
     {
         rigidbody = gameObject.GetComponent < Rigidbody > ( );
-        death_time = Time.time + lifetime;
     }
 
     void Update ( )
     {
-        if ( Time.time >= death_time )
-            GameObject.Destroy ( gameObject );
+        if ( ( Time.time - spawn_time ) >= lifetime )
+        {
+            gameObject.SetActive ( false );
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate ( )
     {
         rigidbody.MovePosition ( transform.position + ( transform.forward * projectile_speed * Time.deltaTime) );
+    }
+
+    void OnTriggerEnter ( Collider other )
+    {
+        if ( other.gameObject.tag == "Enemy" )
+        {
+            Debug.Log ( "Hit Object" );
+            other.gameObject.GetComponent < Enemy > ( ).DeductHealth ( damage );
+            gameObject.SetActive ( false );
+        }
+    }
+
+    public void SetSpawnTime ( float time )
+    {
+        spawn_time = time;
+    }
+
+    public void SetDamage ( float amount )
+    {
+        damage = amount;
     }
 }
