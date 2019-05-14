a54b6ae04065db54b23e7ae5ea1b11d825b4ad90
lcdavison <luke.davison@talktalk.net>
2019-05-14 12:20:31 +0100

    Fixed camera rotation judder

--- UnityProject/Assets/Source/Player/CameraController.cs
+++ UnityProject/Assets/Source/Player/CameraController.cs
@@ -8,20 +8,16 @@ public class CameraController : MonoBehaviour
 
     [SerializeField]
     private Vector2 mouse_sensitivity = new Vector2 ( 1.0f, 1.0f );
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
 
     // Update is called once per frame
-    void LateUpdate()
+    void LateUpdate ( )
     {
         rotation.x += Input.GetAxisRaw ( "Mouse X" ) * mouse_sensitivity.x;
         rotation.y += Input.GetAxisRaw ( "Mouse Y" ) * mouse_sensitivity.y;
 
         rotation.y = Mathf.Clamp ( rotation.y, -60.0f, 60.0f );
 
-        transform.rotation = Quaternion.Euler ( rotation.y * -1.0f, rotation.x, 0.0f );
+        transform.localRotation = Quaternion.Euler ( rotation.y * -1.0f, 0.0f, 0.0f );
         transform.parent.rotation = Quaternion.Euler ( 0.0f, rotation.x, 0.0f );
     }
 }
