5676b7a22d089cd8f3a0fc5a9353f6d536f20b62
lcdavison <luke.davison@talktalk.net>
2019-05-14 12:27:11 +0100

    Added keycards for doors

---
+++ UnityProject/Assets/Source/WorldObjects/KeyCard/KeyCard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCard : MonoBehaviour
+{
+    public enum KEYCOLOR
+    {
+            RED = 0x1,
+            BLUE = 0x2
+    };
+
+    [SerializeField]
+    private KEYCOLOR color;
+
+    void OnTriggerEnter ( Collider other )
+    {
+        if ( other.gameObject.tag == "Player" )
+        {
+            GameManager.AddKeyCard ( this );
+            GameObject.Destroy ( this.gameObject );
+        }
+    }
+
+    public KEYCOLOR GetColor ( )
+    {
+        return color;
+    }
+}
---
+++ UnityProject/Assets/Source/WorldObjects/KeyCard/KeyCardAnimate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCardAnimate : MonoBehaviour
+{
+    private Vector3 start_point;
+    private Vector3 end_point;
+    private float delta = 0.0f;
+    private float change = 0.01f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        start_point = end_point = transform.position;
+        end_point.y += 5.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        delta += change;
+
+        if ( delta >= 1.0f )
+            change = -0.01f;
+        else if ( delta <= 0.0f )
+            change = 0.01f;
+
+        Vector3 position = transform.position;
+        position.y = 1.0f + Mathf.Sin ( delta );
+        transform.position = position;
+    }
+}
