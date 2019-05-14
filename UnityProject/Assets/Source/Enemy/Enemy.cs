1cbd133472afabeda31bd9cd238ee44e4017f927
lcdavison <luke.davison@talktalk.net>
2019-05-14 12:28:01 +0100

    Added enemies

---
+++ UnityProject/Assets/Source/Enemy/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform marker;
+
+    public Transform target;
+
+    [SerializeField]
+    private EnemyData data;
+
+    private float health;
+
+    // Start is called before the first frame update
+    void Start ( )
+    {
+        health = data.health;
+    }
+
+    // Update is called once per frame
+    void Update ( )
+    {
+        if ( health <= 0.0f )
+        {
+            GameObject.Destroy ( gameObject );
+        }
+    }
+
+    public void DeductHealth ( float amount )
+    {
+        health -= amount;
+    }
+
+    public float GetHealth ( )
+    {
+        return health;
+    }
+
+    public EnemyData GetData ( )
+    {
+        return data;
+    }
+}
---
+++ UnityProject/Assets/Source/Enemy/EnemyData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "Enemy/New Enemy", fileName = "new_enemy")]
+public class EnemyData : ScriptableObject
+{
+    public string name = "Bandito";
+    public int level = 0;
+    public int health = 50;
+}
