d40ea01bf760afb1545bc30c0ee1aa39a8ad8bb3
lcdavison <luke.davison@talktalk.net>
2019-05-14 12:22:47 +0100

    Added bullet pool and enemy detection

--- UnityProject/Assets/Source/Player/Player.cs
+++ UnityProject/Assets/Source/Player/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -8,41 +9,89 @@ public class Player : MonoBehaviour
     private GameObject weapon_obj;
 
     [SerializeField]
-    private Weapon equipped_weapon;
-
-    [SerializeField]
     private GameObject weapon_offset;
 
     [SerializeField]
     private GameObject projectile;
 
+    [SerializeField]
+    private Slider health_bar;
+
+    [SerializeField]
+    private Weapon equipped_weapon;
+
+    private BulletPool bullet_pool;
+
+    private float max_health = 100.0f;
+    private float health;
+
     private float last_time = 0.0f;
+
+    private Enemy closest_enemy;
+
+    void Awake ( )
+    {
+        bullet_pool = new BulletPool ( projectile, 30 );
+        health = max_health;
+    }
+
+    void Update ( )
+    {
+        RaycastHit hit;
+        if ( Physics.Raycast ( transform.position, transform.forward, out hit, Mathf.Infinity, 1 ) )
+        {
+            if ( hit.collider.gameObject.tag == "Enemy" )
+            {
+                Debug.DrawRay ( transform.position, transform.forward * hit.distance, Color.blue );
+                closest_enemy = hit.collider.gameObject.GetComponent < Enemy > ( );
+            }
+            else
+            {
+                closest_enemy = null;
+            }
+        }
+
+        health_bar.value = health / max_health;
+    }
 
     public void EquipWeapon ( Weapon weapon )
     {
         MeshFilter weapon_mesh = weapon_obj.GetComponent < MeshFilter > ( );
         equipped_weapon = weapon;
-        weapon_mesh.mesh = equipped_weapon.weapon_mesh;
+        weapon_mesh.mesh = equipped_weapon.mesh;
+        weapon_offset.transform.localPosition = weapon.offset;
     }
 
     public void FireWeapon ( )
     {
-        if ( equipped_weapon.automatic )
+        if ( Input.GetKey ( KeyCode.Mouse0 ) )
         {
-            if ( Input.GetKey ( KeyCode.Mouse0 ) )
+            float current_time = Time.time;
+            if ( current_time - last_time >= ( 1.0f / equipped_weapon.fire_rate ) )
             {
-                float current_time = Time.time;
-                if ( current_time - last_time > ( 1 / equipped_weapon.fire_rate ) )
-                {
-                    last_time = current_time;
-                    GameObject.Instantiate ( projectile, weapon_offset.transform.position, weapon_offset.transform.rotation );
-                }
+                last_time = current_time;
+
+                SpawnBullet ( );
             }
         }
-        else
-        {
-            if ( Input.GetKeyDown ( KeyCode.Mouse0 ) )
-                GameObject.Instantiate ( projectile, weapon_offset.transform.position, weapon_offset.transform.rotation );
-        }
+    }
+
+    public Enemy GetClosestEnemy ( )
+    {
+        return closest_enemy;
+    }
+
+    private void SpawnBullet ( )
+    {
+        GameObject bullet = bullet_pool.GetBullet ( );
+
+        bullet.transform.position = weapon_offset.transform.position;
+        bullet.transform.rotation = weapon_offset.transform.rotation;
+
+        Projectile p = bullet.GetComponent < Projectile > ( );
+        p.SetSpawnTime ( Time.time );
+        p.SetDamage ( equipped_weapon.damage );
+
+        bullet.SetActive ( true );
     }
 }
