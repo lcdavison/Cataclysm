13e28d3886c8fd5929f96686fe0a39a66a9a20d6
lcdavison <luke.davison@talktalk.net>
2019-05-14 12:27:30 +0100

    Added keycards for doors

---
+++ UnityProject/Assets/Source/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static List < KeyCard > keycards = new List < KeyCard > ( );
+
+    public static void AddKeyCard ( KeyCard keycard )
+    {
+        keycards.Add ( keycard );
+    }
+}
