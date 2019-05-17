8032d40909db5f0ee808a2086cf7e68f70c2ca9e
lcdavison <luke.davison@talktalk.net>
2019-05-17 13:04:10 +0100

    Added death screen controls

---
+++ UnityProject/Assets/Source/UserInterface/DeathScreen/DeathScreenUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathScreenUI : MonoBehaviour
+{
+    public void OnClickRestart ( )
+    {
+        SceneManager.LoadScene ( "LevelOne" );
+    }
+
+    public void OnClickExit ( )
+    {
+        SceneManager.LoadScene ( "MainMenu" );
+    }
+}
