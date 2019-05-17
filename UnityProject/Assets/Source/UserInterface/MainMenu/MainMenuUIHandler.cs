9544cbc964d9931e3184be3324231bf6832cf58a
lcdavison <luke.davison@talktalk.net>
2019-05-17 13:04:32 +0100

    Added main menu controls

---
+++ UnityProject/Assets/Source/UserInterface/MainMenu/MainMenuUIHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUIHandler : MonoBehaviour
+{
+    public void PlayOnClick ( )
+    {
+        SceneManager.LoadScene ( "LevelOne" );
+    }
+
+    public void ExitOnClick ( )
+    {
+        Application.Quit ( );
+        Debug.Log ( "Quit Clicked" );
+    }
+}
